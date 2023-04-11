a39ede40c629ec4e7b16fe45765ac4ae240a299b
Erin Capuchino <valleerin4@gmail.com>
2023-04-11 11:49:48 +0800

    shorten url api:added

--- src/components/blocks/URL.jsx
+++ src/components/blocks/URL.jsx
@@ -1,6 +1,35 @@
+import axios from 'axios';
+import { useRef, useState} from 'react';
 const Url = () => {
+    const biturl = 'https://api-ssl.bitly.com/v4/shorten';
+    const data = useRef(null)
+    const [newLink,setNewLink] = useState('');
+
+
+    function Submitlink(e) {
+        e.preventDefault()
+        axios.post(biturl, {
+            long_url : data.current.value
+        }, { headers: {
+                'Authorization' : 'Bearer cdc15d4ecb72ebee341b651c0ddfe77dc7b686f4'
+            }
+        }).then((res) => {
+            setNewLink(res.data?.link)
+
+        }).catch((err) => {
+            console.log(err)
+        })
+
+    }
+    console.log(newLink)
     return (
-        <div></div>
+        <div>
+            <form >
+                <input type="text" ref={data}/>
+                <button onClick={Submitlink}>Submit</button>
+            </form>
+            {newLink}
+        </div>
     )
 }
 export default Url;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,5 @@
 import '@/styles/globals.css'
 import Layout from '../components/styles/layout/layout.jsx'
 export default function App({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  )
+  return (<Component {...pageProps} />)
 }
